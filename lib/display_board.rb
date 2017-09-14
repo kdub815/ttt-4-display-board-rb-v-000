f5bbc90501f72919a8ff3b6acecd7eb78c56fc18
Kristen Warnquist <kristen.warnquist@gmail.com>
2017-09-14 13:50:35 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+puts "Welcome to Tic Tac Toe!"
+
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board
